aecf8246caab412612473bbf2b3b7d4f338146b8
MGPasinetti <mg.pasinetti@gmail.com>
2022-02-24 16:21:42 +0100

    emails list created

---
+++ js/mail.js
@@ -0,0 +1,23 @@
+// MAIL
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// Consigli del giorno:
+// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+// 3. si ma noi cosa vogliamo fare?
+// 4. torniamo a scrivere in italiano
+// 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+// Bonus:
+// Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.
+
+
+// INPUT: usermail
+
+// CHECKING:usermail in list?
+
+// OUTPUT: printing message on outcome of the check
+
+// Creo una lista di mail
+let arrMails = ["name@mail.com", "name1@mail.com", "name2@mail.com", "name3@mail.com", "name4@mail.com", "name5@mail.com", "name6@mail.com", "name7@mail.com", "name10@mail.com", "name9@mail.com"]
